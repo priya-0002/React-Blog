38644d80206f4af0cf1c72f19d275e4816dd8524
priya-002 <priya.md0002@gmail.com>
2021-03-13 18:58:29 +0530

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,29 @@
 import {useState} from 'react';
  
 const Home = () => {
+    const[blogs,setBlogs]=useState([
+    {tittle:'my new website',body:'loreum ispsum',author:'robert',id:1},
+    {tittle:'my  website',body:'loreum ispsum',author:'shenoy',id:2},
+    {tittle:'website',body:'loreum ispsum',author:'tagore',id:3},
+   ]);
 
-    const[name,setName]=useState('mario');
-    const[age,setAge]=useState(25);
-
-    const handleClick=()=> {
-        setName('jia ');
-        setAge(30);
-    }
+   
+    
         
         
     
-    const handleClickAgain=(name)=>
-        alert('hello' +'' +name);
-    
+   
     return (
         <div className="home">
-            <h2>Homepge</h2>
-            <p>{name } is {age} years old</p>
-            <button onClick={handleClick}>Click me</button>
-        <button onClick={()=>
-            handleClickAgain('mario')
-        }>Click me Again</button>
+            {blogs.map((blog)=>(
+                <div className="blog-preview" key={blog.id }>
+               <h2>{ blog.tittle}</h2>
+               <h2>{blog.author}</h2>
+               </div>
+            ))
+            }
         </div>
+        
       );
 }
  
