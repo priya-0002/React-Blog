b0ac44f80dd937545347059b722de1c251d093ca
priya-002 <priya.md0002@gmail.com>
2021-03-23 21:46:07 +0530

    error handling

--- src/Home.js
+++ src/Home.js
@@ -4,14 +4,19 @@ import BlogList from  './BlogList';
  
 const Home = () => {
     const[blogs,setBlogs]=useState([]); 
-    const[isPending,setIsPending]=useState(false);
+    const[isPending,setIsPending]=useState(true);
+    const[error,setError]=useState(true);
 
      
  
         useEffect(()=>{  
           setTimeout(()=>{
             fetch('http://localhost:8001/blogs')                                                     
-            .then((res)=>{                                                                              
+            .then((res)=>{   
+              console.log(res)
+              if(!res.ok)   {
+              throw Error('could not fetch for that api');
+              }                                                                        
               return res.json();
             })
             .then((result)=>{
@@ -19,13 +24,19 @@ const Home = () => {
               console.log(result);
               setBlogs(result)
               console.log(blogs)
-             setIsPending(true)
+             setIsPending(false)
+             setError(null)
       
             })
+            .catch(err=>{
+              setIsPending(false)
+              setError(err.message)
+            
+            })
                 
-          }
+          },1000
 
-          )                                                                            
+          )
         
         },[]);
      
@@ -33,7 +44,8 @@ const Home = () => {
    
     return (
       <div className="home">
-      {isPending?<BlogList blogers={blogs}/>:<h1> loading..</h1>
+        {error?<h1> {error}</h1>:<h1> error free message</h1>}
+      {isPending?<h1> loading..</h1>:<BlogList blogers={blogs}/>
         
         }
         </div>
