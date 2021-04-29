1af9a10a8789a37edf74b63f5c7e236dcd10f8d8
priya-002 <priya.md0002@gmail.com>
2021-04-29 15:05:15 +0530

    adding blog

--- src/Create.js
+++ src/Create.js
@@ -1,17 +1,25 @@
 
 import { useState } from "react";
-import "./navbar.css"
 
 const Create = () => {
-  const [title, setTitle] = useState('');
+  const [tittle, setTittle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('mario');
+  const[isPending,setIsPending]=useState('false');
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const blog = { title, body, author };
+    const blog = { tittle, body, author };
+    setIsPending(true);
 
-    console.log(blog);
+    fetch('http://localhost:8003/blogs/', {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog)
+    }).then(() => {
+      console.log('new blog added');
+      setIsPending(false);
+    })
   }
 
   return (
@@ -23,8 +31,8 @@ const Create = () => {
         <input 
           type="text" 
           required 
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          value={tittle}
+          onChange={(e) => setTittle(e.target.value)}
         />
         <label>Blog body:</label>
         <textarea
@@ -37,10 +45,10 @@ const Create = () => {
           value={author}
           onChange={(e) => setAuthor(e.target.value)}
         >
-          <option value="mario">mario</option>
-          <option value="yoshi">yoshi</option>
+          <option value="Robin">Robin</option>
+          <option value="Shenoy">Shenoy</option>
         </select>
-        <button>Add Blog</button>
+       {!isPending?<button>Add Blog</button>:<button>Adding Blog</button>} 
       </form>
     </div>
   );
